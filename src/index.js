6cc78065b7e4d67f4f84b54987a0313fdf062ad6
Kemal Akkoyun <kakkoyun@gmail.com>
2016-07-01 21:08:50 +0300

    Configure redux-dev-tools.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import App from "./components/app";
+import reducers from "./reducers";
 
-import App from './components/app';
-import reducers from './reducers';
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()((window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore));
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
